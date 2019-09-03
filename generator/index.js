9ef506a8905c21708f0d095092a659a011297d62
Perki <pm@perki.com>
2019-09-03 18:24:55 +0200

    cleaned

--- generator/index.js
+++ generator/index.js
@@ -24,16 +24,6 @@ var pryvHF = {
 };
 
 
-window.mobileAndTabletcheck = function() {
-  var check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-
-  window.alert('isTablet :' + check);
-  console.log("XXXXXX", check)
-  return check;
-};
-
-
 /*globals document, pryv*/
 (function () {
 
@@ -54,38 +44,38 @@ window.mobileAndTabletcheck = function() {
   var previousTick = Date.now();
   var sentCount = 0;
 
-  //if ( window.mobileAndTabletcheck()) {
-    window.addEventListener('deviceorientation', function (event) {
+  window.addEventListener('deviceorientation', function (event) {
 
-      var now = Date.now() / 1000;
-      gLabel.innerHTML =  event.gamma;
-      bLabel.innerHTML =   event.beta;
+    var now = Date.now() / 1000;
+    gLabel.innerHTML = event.gamma;
+    bLabel.innerHTML = event.beta;
 
-      pryvHF.measures.orientationGamma.buffer.push([now, event.gamma]);
-      pryvHF.measures.orientationBeta.buffer.push([now, event.beta]);
-
-      counter += 2;
-    });
+    pryvHF.measures.orientationGamma.buffer.push([now, event.gamma]);
+    pryvHF.measures.orientationBeta.buffer.push([now, event.beta]);
 
 
-  //} else { 
+
+    counter += 2;
+  });
 
 
-    document.onmousemove = function (event) {
-      var now = Date.now() / 1000;
-      xLabel.innerHTML =  event.pageX;
-      yLabel.innerHTML =  event.pageY;
 
-      pryvHF.measures.mouseX.buffer.push([now, event.pageX]);
-      pryvHF.measures.mouseY.buffer.push([now, event.pageY]);
-      counter += 2;
-    };
+  document.onmousemove = function (event) {
+    var now = Date.now() / 1000;
+    xLabel.innerHTML = event.pageX;
+    yLabel.innerHTML = event.pageY;
 
-  //}
+    pryvHF.measures.mouseX.buffer.push([now, event.pageX]);
+    pryvHF.measures.mouseY.buffer.push([now, event.pageY]);
+
+    counter += 2;
+  };
+
+
 
   function sampleHz() {
     var now = Date.now();
-    frequencyLabel.innerHTML = Math.round(counter * 1000 /  (now - previousTick));
+    frequencyLabel.innerHTML = Math.round(counter * 1000 / (now - previousTick));
     dataBufferLabel.innerHTML = 0; // xBuffer.length;
     counter = 0;
     previousTick = now;
@@ -144,7 +134,7 @@ function postBatch(connection, measures, done) {
 
   for (var key in measures) {
     // skip loop if the property is from prototype
-    if (! measures.hasOwnProperty(key)) { continue; }
+    if (!measures.hasOwnProperty(key)) { continue; }
 
 
     // console.log(key, measures[key].event, measures[key].buffer.length);
@@ -164,7 +154,7 @@ function postBatch(connection, measures, done) {
     }
   }
 
-  if (sendCount === 0) { return done(null, null, 0);}
+  if (sendCount === 0) { return done(null, null, 0); }
 
   connection.request({
     withoutCredentials: true,
@@ -197,17 +187,17 @@ function postSerie(connection, event, points, done) {
 function stdPlotly(key, type, name) {
   var data = {};
   data[type] = {
-     plotKey: key,
-      trace: {
-        type: 'scatter',
-        name: name,
-        mode: 'lines',
-        connectgaps: 0
-      }
+    plotKey: key,
+    trace: {
+      type: 'scatter',
+      name: name,
+      mode: 'lines',
+      connectgaps: 0
+    }
 
   }
 
-  return { 'app-web-plotly' : data };
+  return { 'app-web-plotly': data };
 }
 
 function setupConnection(connection) {
@@ -216,59 +206,59 @@ function setupConnection(connection) {
   var postData;
   var resultTreatment;
 
-    postData = [
-      // MOUSE
-      {
-        method: 'streams.create',
-        params: {
-          id: 'hfdemo-mouse-x',
-          name: 'Mouse-X',
-          parentId: 'hfdemo'
+  postData = [
+    // MOUSE
+    {
+      method: 'streams.create',
+      params: {
+        id: 'hfdemo-mouse-x',
+        name: 'Mouse-X',
+        parentId: 'hfdemo'
+      }
+    },
+    {
+      method: 'streams.update',
+      params: {
+        id: 'hfdemo-mouse-x',
+        update: {
+          clientData: stdPlotly('Mouse', 'count/generic', 'X')
         }
-      },
-      {
-        method: 'streams.update',
-        params: {
-          id: 'hfdemo-mouse-x',
-          update: { 
-            clientData: stdPlotly('Mouse', 'count/generic', 'X')
-          }
+      }
+    },
+    {
+      method: 'events.create',
+      params: {
+        streamId: 'hfdemo-mouse-x',
+        type: 'series:count/generic',
+        description: 'Holder for x mouse position'
+      }
+    },
+    {
+      method: 'streams.create',
+      params: {
+        id: 'hfdemo-mouse-y',
+        name: 'Mouse-Y',
+        parentId: 'hfdemo'
+      }
+    },
+    {
+      method: 'streams.update',
+      params: {
+        id: 'hfdemo-mouse-y',
+        update: {
+          clientData: stdPlotly('Mouse', 'count/generic', 'Y')
         }
-      },
-      {
-        method: 'events.create',
-        params: {
-          streamId: 'hfdemo-mouse-x',
-          type: 'series:count/generic',
-          description: 'Holder for x mouse position'
-        }
-      },
-      {
-        method: 'streams.create',
-        params: {
-          id: 'hfdemo-mouse-y',
-          name: 'Mouse-Y',
-          parentId: 'hfdemo'
-        }
-      },
-      {
-        method: 'streams.update',
-        params: {
-          id: 'hfdemo-mouse-y',
-          update: { 
-            clientData: stdPlotly('Mouse', 'count/generic', 'Y')
-          }
-        }
-      },
-      {
-        method: 'events.create',
-        params: {
-          streamId: 'hfdemo-mouse-y',
-          type: 'series:count/generic',
-          description: 'Holder for y mouse position'
-        }
-      },
-      {
+      }
+    },
+    {
+      method: 'events.create',
+      params: {
+        streamId: 'hfdemo-mouse-y',
+        type: 'series:count/generic',
+        description: 'Holder for y mouse position'
+      }
+    },
+    {
       method: 'streams.create',
       params: {
         id: 'hfdemo-orientation-gamma',
@@ -318,7 +308,7 @@ function setupConnection(connection) {
         description: 'Holder for device beta'
       }
     }
-    ];
+  ];
 
   resultTreatment = [
     null,
@@ -333,20 +323,20 @@ function setupConnection(connection) {
       pryvHF.measures.mouseY.event = result.event;
       console.log('handle yEvent set', result.event);
     },
-      null,
-      null,
-      function handleCreateEventGamma(result) {
-        pryvHF.measures.orientationGamma.event = result.event;
-        console.log('handle gammaEvent set', result.event);
-      },
-      null,
-      null,
-      function handleCreateEventBeta(result) {
-        pryvHF.measures.orientationBeta.event = result.event;
-        console.log('handle betaEvent set', result.event);
-      }
-    ];
-  
+    null,
+    null,
+    function handleCreateEventGamma(result) {
+      pryvHF.measures.orientationGamma.event = result.event;
+      console.log('handle gammaEvent set', result.event);
+    },
+    null,
+    null,
+    function handleCreateEventBeta(result) {
+      pryvHF.measures.orientationBeta.event = result.event;
+      console.log('handle betaEvent set', result.event);
+    }
+  ];
+
 
 
 
@@ -395,7 +385,7 @@ function getRegisterURL() {
 
 var customRegisterUrl = getRegisterURL();
 if (customRegisterUrl) {
-  pryv.Auth.config.registerURL = {host: customRegisterUrl, 'ssl': true};
+  pryv.Auth.config.registerURL = { host: customRegisterUrl, 'ssl': true };
 }
 
 /**
@@ -403,8 +393,8 @@ if (customRegisterUrl) {
  */
 function getSettingsFromURL() {
   var settings = {
-    username : pryv.utility.urls.parseClientURL().parseQuery().username,
-    domain : pryv.utility.urls.parseClientURL().parseQuery().domain,
+    username: pryv.utility.urls.parseClientURL().parseQuery().username,
+    domain: pryv.utility.urls.parseClientURL().parseQuery().domain,
     auth: pryv.utility.urls.parseClientURL().parseQuery().auth
   };
 
